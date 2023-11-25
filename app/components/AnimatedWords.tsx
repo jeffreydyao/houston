8f399746665677faa93adad40eb3166b5726948c
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2023-11-25 23:11:23 +1100

    experiment: animated words

---
+++ app/components/AnimatedWords.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useMemo, useState } from "react";
+
+const words = [
+  "explorer",
+  "gymbro",
+  "runner",
+  "hip-hop head",
+  "designer",
+  "student",
+];
+
+export default function AnimatedWords() {
+  const [animating, setAnimating] = useState(false);
+  const [startIndex, setStartIndex] = useState<number>(0);
+
+  const window = useMemo(() => {
+    const start = startIndex % words.length;
+    const end = (start + 2) % words.length;
+    if (end > start) {
+      return words.slice(start, end);
+    } else {
+      return [...words.slice(start), ...words.slice(0, end)];
+    }
+  }, [startIndex]);
+
+  function nextWord() {
+    setAnimating(true);
+    setTimeout(() => {
+      setAnimating(false);
+      setStartIndex(startIndex + 1);
+    }, 150);
+  }
+
+  return (
+    <>
+      <span aria-live="polite" className="hidden">
+        {window?.[0] ?? words[0]}
+      </span>
+      <div className="flex gap-4 items-center">
+        <ul
+          className="h-[30px] items overflow-hidden"
+          style={{
+            WebkitMaskImage:
+              "linear-gradient(180deg, transparent 5%, rgba(0, 0, 0, 1) 20%, rgba(0, 0, 0, 1) 80%, transparent 95%)",
+          }}
+        >
+          {window?.map((word, i) => (
+            <li
+              key={word}
+              className={`font-semibold text-xl ${
+                i === 0
+                  ? `transition-all transform-gpu ${
+                      animating ? "-mt-7 opacity-0" : "mt-0 opacity-100"
+                    }`
+                  : !animating && "hidden"
+              }`}
+            >
+              <p> {word}</p>
+            </li>
+          ))}
+        </ul>
+
+        <button
+          disabled={animating}
+          onClick={nextWord}
+          className="font-medium underline text-blue-600 text-sm"
+        >
+          Next word
+        </button>
+      </div>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import AnimatedWords from "./components/AnimatedWords";
+
 export default function Home() {
   return (
     <section>
-      <h1 className="font-bold text-xl">Hi, I'm Jeffrey Yao</h1>
-      <p className="font-bold text-xl">a software engineer</p>
-      <p className="font-bold text-xl">and photographer</p>
+      <h1 className="font-semibold text-xl">Hi, I'm Jeffrey Yao</h1>
+      <p className="font-semibold text-xl">a software engineer</p>
+      <div className="font-semibold text-xl inline-flex gap-1">
+        <p>and</p>
+        <AnimatedWords />
+      </div>
     </section>
   );
 }

91c308a4ab63ce331d5a9c8c3413d817af511b80
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2023-11-27 20:45:29 +1100

    refactor: switch to harmony p3 color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import harmonyPalette from "@evilmartians/harmony/tailwind";
 
 const config: Config = {
   content: [
@@ -11,23 +12,8 @@ const config: Config = {
       fontFamily: {
         sans: ["var(--font-inter-var)", "system-ui", "sans-serif"],
       },
-      colors: {
-        // https://www.tints.dev/blue/33C924
-        horizon: {
-          50: "color(display-p3 0.898 0.941 1 / <alpha-value>)",
-          100: "color(display-p3 0.8 0.882 1 / <alpha-value>)",
-          200: "color(display-p3 0.6 0.765 1 / <alpha-value>)",
-          300: "color(display-p3 0.4 0.651 1 / <alpha-value>)",
-          400: "color(display-p3 0.2 0.533 1 / <alpha-value>)",
-          500: "color(display-p3 0 0.416 1 / <alpha-value>)",
-          600: "color(display-p3 0 0.333 0.8 / <alpha-value>)",
-          700: "color(display-p3 0 0.251 0.6 / <alpha-value>)",
-          800: "color(display-p3 0 0.165 0.4 / <alpha-value>)",
-          900: "color(display-p3 0 0.082 0.2 / <alpha-value>)",
-          950: "color(display-p3 0 0.043 0.098 / <alpha-value>)",
-        },
-      },
     },
+    colors: harmonyPalette,
   },
   plugins: [],
 };
